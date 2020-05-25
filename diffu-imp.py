26a70d40d0c5bc8fc73bb7683ee3f1d28539a4ec
Johny <jhonyal58@gmail.com>
2020-05-25 18:57:21 -0500

    Add diffu-imp.py

---
+++ diffu-imp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.sparse
+import scipy.sparse.linalg
+
+import imageio
+
